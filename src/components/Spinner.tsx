e14410c31ebd6d55b3948a50b147bfa1d337a565
vlad1slove1 <vlad5114@gmail.com>
2023-10-04 23:51:00 +0300

    created Spinner element

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CircularProgress } from '@nextui-org/react'
+
+export default function Spinner (): React.JSX.Element {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+      <CircularProgress size="lg" aria-label="load spinner" label="Loading data..." />
+    </div>
+  )
+};
