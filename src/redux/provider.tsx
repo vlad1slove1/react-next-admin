f83452a0efc7badbf6bcfadc681f9e193df29d3c
vlad1slove1 <vlad5114@gmail.com>
2023-10-04 23:52:20 +0300

    App wrapped by NextUIProvider

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -4,7 +4,14 @@
 
 import store from './store'
 import { Provider } from 'react-redux'
+import { NextUIProvider } from '@nextui-org/react'
 
 export default function Providers ({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <NextUIProvider>
+      <Provider store={store}>
+        {children}
+      </Provider>
+    </NextUIProvider>
+  )
 }
