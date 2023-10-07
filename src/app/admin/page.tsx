92ae820cbf13dc9fd4baa7725afaa064798bc31a
vlad1slove1 <vlad5114@gmail.com>
2023-10-07 23:01:47 +0300

    updated data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,59 +1,110 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useGetOrdersQuery } from '@/redux/services/supabaseApi'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 
+import NewOrderButton from '@/components/NewOrderButton'
 import Spinner from '@/components/Spinner'
 import Box from '@mui/material/Box'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/DeleteOutlined'
 import { DataGrid } from '@mui/x-data-grid/DataGrid'
-import { type GridColDef } from '@mui/x-data-grid'
+import {
+  type GridColDef,
+  type GridRowModesModel,
+  type GridRowId,
+  GridActionsCellItem,
+  GridRowModes
+} from '@mui/x-data-grid'
 
 export default function AdminPage (): React.JSX.Element {
-  const { data, error } = useGetOrdersQuery(null)
-  const [selectedItems, setSelectedItems] = useState([])
+  const { data, error, isLoading } = useGetOrdersQuery(null)
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({})
 
-  const handleSelection = (params: any): void => {
-    console.log(params)
-    setSelectedItems(params)
-  }
-
-  if (data == null) {
+  if (isLoading) {
     return (
       <Spinner />
     )
   }
 
-  // console.log(selectedItems)
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } })
+  }
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.id !== id))
+  }
 
   const columns: GridColDef[] = [
-    { field: 'createdAt', headerName: 'Date', headerAlign: 'center', align: 'center', flex: 1 },
-    { field: 'author', headerName: 'Author', headerAlign: 'center', align: 'center', flex: 2 },
-    { field: 'price', headerName: 'Price', headerAlign: 'center', align: 'center', flex: 1 },
-    { field: 'company', headerName: 'Company', headerAlign: 'center', align: 'center', flex: 2 },
-    { field: 'address', headerName: 'Address', headerAlign: 'center', flex: 3 },
-    { field: 'contact', headerName: 'Contact', headerAlign: 'center', align: 'center', flex: 2 },
-    { field: 'email', headerName: 'Email', headerAlign: 'center', align: 'center', flex: 2 },
-    { field: 'about', headerName: 'Description', headerAlign: 'center', sortable: false, flex: 3 },
-    { field: 'id', headerName: 'id', headerAlign: 'center', align: 'center', flex: 1 }
+    { field: 'createdAt', headerName: 'Date', headerAlign: 'center', align: 'center', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'author', headerName: 'Author', headerAlign: 'center', align: 'center', flex: 2, headerClassName: 'super-app-theme--header' },
+    { field: 'price', headerName: 'Price', headerAlign: 'center', align: 'center', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'company', headerName: 'Company', headerAlign: 'center', align: 'center', flex: 2, headerClassName: 'super-app-theme--header' },
+    { field: 'address', headerName: 'Address', headerAlign: 'center', flex: 3, headerClassName: 'super-app-theme--header' },
+    { field: 'contact', headerName: 'Contact', headerAlign: 'center', align: 'center', flex: 2, headerClassName: 'super-app-theme--header' },
+    { field: 'email', headerName: 'Email', headerAlign: 'center', align: 'center', flex: 2, headerClassName: 'super-app-theme--header' },
+    { field: 'about', headerName: 'Description', headerAlign: 'center', sortable: false, flex: 3, headerClassName: 'super-app-theme--header' },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      flex: 1,
+      cellClassName: 'actions',
+      headerClassName: 'super-app-theme--header',
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            key={id}
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            key={id}
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />
+        ]
+      }
+    }
   ]
 
   return (
-    <Box sx={{ width: '100%', background: 'white' }}>
-      <DataGrid
-        rows={data}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 15 }
+    <Box>
+      <Box sx={{ backgroundColor: 'rgba(237, 231, 225)' }}>
+        <NewOrderButton />
+      </Box>
+
+      <Box
+        sx={{
+          width: '100%',
+          background: 'white',
+          '& .super-app-theme--header': {
+            backgroundColor: 'rgba(237, 231, 225)',
+            fontSize: 'large'
           }
         }}
-        pageSizeOptions={[15, 30, 45]}
-        checkboxSelection
-        disableRowSelectionOnClick
-        rowSelectionModel={selectedItems}
-        onRowSelectionModelChange={handleSelection}
-      />
+      >
+        <DataGrid
+          rows={data}
+          columns={columns}
+          rowModesModel={rowModesModel}
+          // slots={{
+          //   toolbar: NewOrderButton
+          // }}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 15 }
+            }
+          }}
+          pageSizeOptions={[15, 30, 45]}
+        />
+      </Box>
     </Box>
   )
 }
