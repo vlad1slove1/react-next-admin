14beb019434142b16eb9a1d52ddd58426896e0da
vlad1slove1 <vlad5114@gmail.com>
2023-10-30 22:52:21 +0300

    added types & added createOrder endpoint

--- src/redux/services/supabaseApi.ts
+++ src/redux/services/supabaseApi.ts
@@ -1,15 +1,42 @@
-import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import supabase from '@/supabase'
 
+interface IOrder {
+  about: string | null
+  address: string | null
+  author: string | null
+  company: string | null
+  contact: string | null
+  createdAt: string | null
+  email: string | null
+  price: string | null
+  id?: number | null
+}
+
 export const supabaseApi = createApi({
-  baseQuery: fakeBaseQuery(),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL }),
   endpoints: (builder) => ({
-    getOrders: builder.query({
+    getOrders: builder.query<IOrder[], null>({
       queryFn: async () => {
         const { data, error } = await supabase
           .from('orders')
           .select('*')
-          .order('id', { ascending: true })
+          .order('id', { ascending: false })
+
+        if (error != null) {
+          throw new Error(error.message)
+        }
+
+        return { data }
+      }
+    }),
+    createOrder: builder.mutation({
+      queryFn: async (newOrder) => {
+        const { data, error } = await supabase
+          .from('orders')
+          .insert(newOrder)
+          .select('*')
+          .order('id', { ascending: false })
 
         if (error != null) {
           throw new Error(error.message)
@@ -21,4 +48,4 @@ export const supabaseApi = createApi({
   })
 })
 
-export const { useGetOrdersQuery } = supabaseApi
+export const { useGetOrdersQuery, useCreateOrderMutation } = supabaseApi
