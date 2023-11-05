11e47cf4b62ea31638473a60b4ce19c917ccbfb9
vlad1slove1 <vlad5114@gmail.com>
2023-11-05 20:43:44 +0300

    updated namings in supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-import { type Database } from '@/types/database.types'
+
+import type { Database } from '@/types/database.types'
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  process.env.NEXT_PUBLIC_SECRET_KEY ?? ''
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
 )
 
 export default supabase
