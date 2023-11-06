72f8a3546c09d0fbce205668d28f70cd1232de63
vlad1slove1 <vlad5114@gmail.com>
2023-11-06 23:59:53 +0300

    root page ('/') updated with Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react'
 import Image from 'next/image'
 
-export default function Home () {
+export default function Home (): React.JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
