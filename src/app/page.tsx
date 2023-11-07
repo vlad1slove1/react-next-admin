8259cf5199961f78d1e43e1af626bf0d8d02aeed
vlad1slove1 <vlad5114@gmail.com>
2023-11-07 20:17:29 +0300

    updated root page '/' with new description / background image and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,46 @@
+'use client'
+
 import React from 'react'
+
 import Image from 'next/image'
+import { Link, Code } from '@nextui-org/react'
+
+import homePageBackground from '../../public/homePageBackground.jpeg'
 
 export default function Home (): React.JSX.Element {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="z-0">
+        <Image
+          src={homePageBackground}
+          alt="Home page background"
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: 'cover'
+          }}
+        />
+      </div>
+
+      <div className="z-10 max-w-5xl w-full items-center justify-between align-center font-mono lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center bg-gradient-to-b pb-6 pt-8 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:p-4">
+          You&apos;re on a root page. To manage admin data, link to&nbsp;
+          {' '}
+          <Link href={'/admin'} underline="hover">admin page</Link>
         </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://nextjs.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            Created on{' '}
             <Image
-              src="/vercel.svg"
+              src="/next.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -27,6 +49,20 @@ export default function Home (): React.JSX.Element {
             />
           </a>
         </div>
+      </div>
+
+      <div className="z-10 max-w-3xl w-full items-center justify-center align-center font-semibold lg:flex md:w-fit">
+        <p className="mt-40 justify-center text-justify border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          This is a simple admin panel, used to improve my skills at creating fullstack app.<br/><br/>
+          I tried to combine different technologies all by one application, maintained by Next.js.
+          Main data stored at <Link href={'https://supabase.com/'} color="foreground" className="underline decoration-2 decoration-sky-500 text-white">supabase</Link> - this is Postgres database, with Authentication, instant APIs and Realtime subscriptions. Supabase works seamlessly with Next.js and React applications.
+          App fetch data with <Link href={'https://redux-toolkit.js.org/rtk-query/overview'} color="foreground" className="underline decoration-2 decoration-sky-500 text-white">Redux RTK Query</Link> and store it to <Link href={'https://redux-toolkit.js.org/tutorials/typescript'} color="foreground" className="underline decoration-2 decoration-sky-500 text-white">Redux Store</Link>.
+          Represented data wrapped and visualized by <Link href={'https://mui.com/x/react-data-grid/'} color="foreground" className="underline decoration-2 decoration-sky-500 text-white">Material UI Data Grid</Link> component.
+          <br/><br/>
+          <Code size="sm">
+            TypeScript / Next.js / React / Redux / Supabase / Material UI / Tailwind
+          </Code>
+        </p>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -38,76 +74,6 @@ export default function Home (): React.JSX.Element {
           height={37}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
